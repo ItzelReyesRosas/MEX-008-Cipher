006590a53b33daddba54712c9c530bbbd91e5117
ItzelReyesRosas <imrr87@gmail.com>
2019-06-14 05:36:20 -0500

    Cambios en los archivos index, contactos y style

--- src/index.js
+++ src/index.js
@@ -3,23 +3,25 @@
 
 const inputName = document.getElementById('ingresar');*/
 let offset = document.getElementById('input-offset');
-const botonCodifica = document.getElementById('boton-codifica');
-const botonIngreso = document.getElementById('boton-ingreso');
 const inputName =  document.getElementById('input-name');
 const bienvenido = document.getElementById('welcome');
 
 
-const ingreso = ()=>{
-     window.location="contactos.html";
-     //location.href="contactos.html";
-     bienvenido.innerHTML = 'Bienvenido(a)' + inputName.value; //bienvenido.innerHTML = inputName.value + '</br>'+ 'Bienvenido';
+console.log(inputName);
+
+const saluda = () => {
+
+
+     bienvenido.innerHTML = 'Bienvenido(a)' + ' ' + inputName.value; //bienvenido.innerHTML = inputName.value + '</br>'+ 'Bienvenido';
+     //window.location="contactos.html";
     // console.log(offset.value);
+    //location.href="contactos.html";
 }
 
 const codifica = () => {
-         window.location="chatsDos.html";
+         window.location="chats2.html";
 
 }
 
-botonIngreso.addEventListener("click", ingreso);
-//botonCodifica.addEventListener("click", codifica);
+
+   inputName.addEventListener('click', saluda);
