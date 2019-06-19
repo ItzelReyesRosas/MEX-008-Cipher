c026116f653484f7b97cc60d6696e51784c8fd31
ItzelReyesRosas <imrr87@gmail.com>
2019-06-19 11:22:11 -0500

    Código js de contactos para hacer funcional el cifrado y descifrado

--- src/index.js
+++ src/index.js
@@ -1,27 +1,65 @@
+
+//let inputName =  document.getElementById('input-name');
+//let bienvenido = document.getElementById('welcome');
+//const botonIngresar = document.getElementById('ingresar');
+
+
+//botonIngresar.addEventListener("click", event =>{
+  //  location.href="contactos.html";
+    //bienvenido.innerHTML = "Bienvenid@ " + inputName.value ;
+    //event.preventDefault();
+  //});
+
+
 /*document.getElementById('ingresar').addEventListener('clic');
 
 
 const inputName = document.getElementById('ingresar');*/
-let offset = document.getElementById('input-offset');
-const inputName =  document.getElementById('input-name');
-const bienvenido = document.getElementById('welcome');
+//let offset = document.getElementById('offset');
+//const inputName =  document.getElementById('input-name');
+//const bienvenido = document.getElementById('welcome');
+
+const botonTexto = document.getElementById('boton-texto');
+botonTexto.addEventListener("click", event =>{
+  let textoChat = document.getElementById('texto-chat');
+  let textoOriginal = document.getElementById('texto-original').value;
+  textoChat.value = textoOriginal;
+  event.preventDefault();
+});
+
+const botonCodifica = document.getElementById('boton-codifica');
+  botonCodifica.addEventListener("click", event => {
+  let textoOriginal = document.getElementById('texto-original').value;
+  let textoCifrado = document.getElementById('texto-cifrado');
+  let offset = document.getElementById('offset').value;
+  textoCifrado.value = cipher.encode(parseInt(offset), textoOriginal);
+  event.preventDefault();
+   });
 
 
-console.log(inputName);
 
-const saluda = () => {
+   const botonDescifra = document.getElementById('boton-descifra');
+     botonDescifra.addEventListener("click", event => {
+     let textoCifrado = document.getElementById('texto-cifrado').value;
+     let textoDescifrado = document.getElementById('texto-descifrado');
+     let offset = document.getElementById('offset').value;
+     textoDescifrado.value = cipher.decode(parseInt(offset), textoCifrado);
+     event.preventDefault();
+      });
 
 
-     bienvenido.innerHTML = 'Bienvenido(a)' + ' ' + inputName.value; //bienvenido.innerHTML = inputName.value + '</br>'+ 'Bienvenido';
+
+//console.log(inputName);
+
+//const saluda = () => {
+
+
+    // bienvenido.innerHTML = 'Bienvenido(a)' + ' ' + inputName.value; //bienvenido.innerHTML = inputName.value + '</br>'+ 'Bienvenido';
      //window.location="contactos.html";
     // console.log(offset.value);
     //location.href="contactos.html";
-}
-
-const codifica = () => {
-         window.location="chats2.html";
-
-}
 
 
-   inputName.addEventListener('click', saluda);
+
+
+   //inputName.addEventListener('click', saluda);
