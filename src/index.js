106474daca4d7f3a0f37095794d45fc610d31059
ItzelReyesRosas <imrr87@gmail.com>
2019-06-20 11:35:38 -0500

    Versión final index js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,24 @@
 
 let inputName =  document.getElementById('input-name');
-let bienvenido = document.getElementById('welcome');
+let mensajes = document.getElementById('mensajes-usuario');
 const botonIngresar = document.getElementById('boton-ingresar');
+const botonTexto = document.getElementById('boton-texto');
+const botonCodifica = document.getElementById('boton-codifica');
+const botonDescifra = document.getElementById('boton-descifra');
 
 
 botonIngresar.addEventListener("click", event =>{
-    bienvenido.innerHTML = "Bienvenid@ " + inputName.value ;
+    mensajes.innerHTML = "&nbsp&nbsp&nbspMensajes de "  + inputName.value  ;
     let segundaParte = document.getElementById('segunda-parte');
     let primeraParte = document.getElementById('primera-parte');
     primeraParte.style.display="none";
     segundaParte.style.display="block";
-
     event.preventDefault();
   });
 
 
-/*document.getElementById('ingresar').addEventListener('clic');
 
 
-const inputName = document.getElementById('ingresar');*/
-//let offset = document.getElementById('offset');
-//const inputName =  document.getElementById('input-name');
-//const bienvenido = document.getElementById('welcome');
-
-const botonTexto = document.getElementById('boton-texto');
 botonTexto.addEventListener("click", event =>{
   let textoChat = document.getElementById('texto-chat');
   let textoOriginal = document.getElementById('texto-original').value;
@@ -31,11 +26,10 @@ botonTexto.addEventListener("click", event =>{
   event.preventDefault();
 });
 
-const botonCodifica = document.getElementById('boton-codifica');
+
   botonCodifica.addEventListener("click", event => {
   let textoOriginal = document.getElementById('texto-original').value;
   let textoCifrado = document.getElementById('texto-cifrado');
-  //let offset = document.getElementById('offset').value;
   let offset = document.getElementById('input-offset').value;
   textoCifrado.value = cipher.encode(parseInt(offset), textoOriginal);
   event.preventDefault();
@@ -43,29 +37,11 @@ const botonCodifica = document.getElementById('boton-codifica');
 
 
 
-   const botonDescifra = document.getElementById('boton-descifra');
+
      botonDescifra.addEventListener("click", event => {
      let textoCifrado = document.getElementById('texto-cifrado').value;
      let textoDescifrado = document.getElementById('texto-descifrado');
-     //let offset = document.getElementById('offset').value;
      let offset = document.getElementById('input-offset').value;
      textoDescifrado.value = cipher.decode(parseInt(offset), textoCifrado);
      event.preventDefault();
       });
-
-
-
-//console.log(inputName);
-
-//const saluda = () => {
-
-
-    // bienvenido.innerHTML = 'Bienvenido(a)' + ' ' + inputName.value; //bienvenido.innerHTML = inputName.value + '</br>'+ 'Bienvenido';
-     //window.location="contactos.html";
-    // console.log(offset.value);
-    //location.href="contactos.html";
-
-
-
-
-   //inputName.addEventListener('click', saluda);
